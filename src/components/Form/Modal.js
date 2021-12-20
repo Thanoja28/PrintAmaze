55df4345b5a276dc663791c788985a1a66643633
Thanoja Thangaraja <thanujathanuja18@yahoo.com>
2021-12-20 00:54:08 +0000

    made changes to Modal

--- src/components/Form/Modal.js
+++ src/components/Form/Modal.js
@@ -5,32 +5,6 @@ import { MdClose } from 'react-icons/md';
 import  Form  from './Form';
 import './Modal.css';
 
-// const Background = styled.section`
-//   width: 100%;
-//   height: 100%;
-//   background: rgba(0, 0, 0, 0.8);
-//   position: fixed;
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-// `;
-
-// const ModalWrapper = styled.section`
-//   width: 800px;
-//   height: 500px;
-//   box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
-//   background: #fff;
-//   color: #000;
-//   display: grid;
-//   grid-template-columns: 1fr 1fr;
-//   position: relative;
-//   z-index: 10;
-//   border-radius: 10px;
-// `;
-
-
-
-
 const CloseModalButton = styled(MdClose)`
   cursor: pointer;
   position: absolute;
